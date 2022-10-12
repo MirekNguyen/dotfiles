72f8572dd52acd0e82a5bf43b0573a76b46aae7f
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-10-12 13:38:04 +0200

    feat(fish): add bat alias

--- config.fish
+++ config.fish
@@ -21,6 +21,7 @@ alias notes='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Notes'
 alias youtube='cd ~/Downloads/Youtube'
 alias soffice='/Applications/_Casks/LibreOffice.app/Contents/MacOS/soffice'
 alias w='webtorrent download --out "$HOME"/Downloads'
+alias bat='bat --style=plain --theme gruvbox-dark --color=always'
 
 # pnpm
 set -gx PNPM_HOME "/Users/mireknguyen/Library/pnpm"
