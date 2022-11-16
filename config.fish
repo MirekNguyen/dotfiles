ebd404cb572f60a6f42fb0b50eda0d904f3f4e10
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-11-16 19:04:51 +0100

    refactor(fish): update ls & aliases

--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
 export HOMEBREW_CASK_OPTS="--appdir=/Applications/_Casks"
+
+export LESSHISTFILE=-
+
 set fish_greeting
 set -gx EDITOR nvim 
 set -gx TERM xterm-256color
@@ -8,21 +11,20 @@ alias prog="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Programming/"
 alias web="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Programming/Websites"
 alias d='cd ~/Desktop'
 alias rasp="ssh pi@mirekng.com"
+alias r="ssh pi@mirekng.com"
 alias web='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Programming/Websites/'
 alias scripts='cd ~/.local/bin'
 alias fish-reload='source ~/.config/fish/config.fish'
-alias ls='exa --icons -g --oneline'
-alias la='exa --icons -g -a --oneline'
-alias ll='exa --icons -g -l'
-alias lla='exa --icons -g -la'
+alias ls='exa -g --oneline'
+alias la='exa -g -a --oneline'
+alias ll='exa -g -l'
+alias lla='exa -g -la'
 alias v='nvim'
 alias do='cd ~/Downloads/'
 alias ido='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads'
 alias notes='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Notes'
-alias youtube='cd ~/Downloads/Youtube'
 alias soffice='/Applications/_Casks/LibreOffice.app/Contents/MacOS/soffice'
 alias w='webtorrent download --out "$HOME"/Downloads'
-alias bat='bat --style=plain --theme gruvbox-dark --color=always'
 alias update='brew upgrade && brew autoremove && brew cleanup && fisher update &>/dev/null & nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"'
 alias cleanup='brew autoremove && brew cleanup && npm cache clean --force && pnpm store prune'
 
