41d5a80e11a120019894b535687353989199d7ce
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-11-26 01:50:01 +0100

    refactor(fish): remove redundant aliases

--- config.fish
+++ config.fish
@@ -7,12 +7,7 @@ set -gx EDITOR nvim
 set -gx TERM xterm-256color
 
 alias icloud="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/"
-alias prog="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Programming/"
-alias web="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Programming/Websites"
 alias d='cd ~/Desktop'
-alias rasp="ssh pi@mirekng.com"
-alias r="ssh pi@mirekng.com"
-alias web='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Programming/Websites/'
 alias scripts='cd ~/.local/bin'
 alias fish-reload='source ~/.config/fish/config.fish'
 alias ls='exa -g --oneline'
@@ -23,5 +18,4 @@ alias v='nvim'
 alias do='cd ~/Downloads/'
 alias ido='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads'
 alias notes='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Notes'
-alias soffice='/Applications/_Casks/LibreOffice.app/Contents/MacOS/soffice'
 alias w='webtorrent download --out "$HOME"/Downloads'
