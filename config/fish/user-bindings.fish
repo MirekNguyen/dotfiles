5fa85fea3ed2359061f466eddbfb7ec73ceda317
Mirek Nguyen <nguyemir@fit.cvut.cz>
2023-06-26 21:45:56 +0200

    feat: gruvbox zathura

--- config/fish/user-bindings.fish
+++ config/fish/user-bindings.fish
@@ -2,4 +2,4 @@ bind \e\[106\;9u "_fzf_find $HOME"
 bind \e\[108\;9u "_fzf_find_search '/Users/mireknguyen/Library/Mobile Documents/com~apple~CloudDocs/notes/'"
 bind \e\[106\;10u '_fzf_find'
 bind \e\[108\;10u '_fzf_find_search'
-bind \e\[91\;9u 'chatgpt'
+bind \e\[105\;9u "cheat-fzf"
