066ffb756069eb03ba8ada8381cbc75baaedb4bd
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-04 00:54:46 +0100

    fix: chatgpt keymap

--- config/fish/user-bindings.fish
+++ config/fish/user-bindings.fish
@@ -1,6 +1,6 @@
 bind \e\[106\;9u "_fzf_find $HOME"
 bind \e\[108\;9u "_fzf_find_search '/Users/mireknguyen/Library/Mobile Documents/com~apple~CloudDocs/notes/'"
-bind \e\[106\;10u '_fzf_find --noignorefile'
-bind \e\[108\;10u '_fzf_find_search'
+bind \e\[106\;10u "_fzf_find --noignorefile"
+bind \e\[108\;10u "_fzf_find_search"
 bind \e\[105\;9u "cheat-fzf"
-bind \e\[112\;10u 'switch_tabs'
+bind \e\[111\;9u "switch_tabs"
