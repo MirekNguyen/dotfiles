be809828c32d694059be2c3a6ace98e1f4b48b03
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-26 22:46:58 +0100

    feat: bottom memory as value

--- config/fish/user-aliases.fish
+++ config/fish/user-aliases.fish
@@ -1,16 +1,21 @@
 alias v='nvim'
 alias w='webtorrent download --out "$HOME"/Downloads'
 alias n "newsboat -q && neomutt -z"
-alias b "btm -b"
-alias btm "btm --color gruvbox"
+alias b "btm --basic"
+alias btm "btm --color gruvbox --mem_as_value --group"
+alias bat "bat --theme=gruvbox --style=numbers --color=always"
+alias batjson "bat --theme=gruvbox --style=numbers --color=always --language=json"
 alias icat="kitty +kitten icat"
-alias libreoffice="soffice"
 alias mans="apropos . | grep \(1\) | sed 's/(1)//g' | fzf | cut -d' ' -f1,1 | xargs -I {} man {}"
-alias mvi="mpv --config-dir=$HOME/.config/mvi &>/dev/null"
+
+# apps
 alias img="open -a \"Preview\""
 alias images="timg --grid=4 --title --center $(fd --max-depth 1 --regex '^*.\.(jpg|png|gif|jpeg|avif|webp|tiff|bmp)' .)"
 alias apps="osascript -e 'tell application \"System Events\" to get the name of (every application process whose background only is false)'"
 alias mpv "mpv --player-operation-mode=pseudo-gui --script-opts=ytdl_hook-ytdl_path=yt-dlp --ytdl-format='bestvideo[height<=?1080]+bestaudio/best'"
+alias libreoffice="soffice"
+
+# copilot
 alias ghs "gh copilot suggest"
 alias ghss "gh copilot suggest -t shell"
 alias ghsgit "gh copilot suggest -t git"
