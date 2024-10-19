577affc79a110a3b33f770f692cd91bfbef12970
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-19 16:48:22 +0200

    feat(fish): revert user bindings

--- config/fish/user-bindings.fish
+++ config/fish/user-bindings.fish
@@ -1,5 +1,4 @@
 bind \e\[106\;9u "_fzf_find $HOME"
 bind \e\[108\;9u "_fzf_find_search '$HOME/.local/mount/onedrive/notes/programming/'"
 bind \e\[106\;10u "_fzf_find --noignorefile"
-# bind \e\[108\;10u "_fzf_find_search"
-bind \e\[107\;9u "_fzf_find_search"
+bind \e\[108\;10u "_fzf_find_search"
