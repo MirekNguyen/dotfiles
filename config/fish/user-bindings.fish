fdb60dc43ad4f77f6e6aeba88a53366731a369ee
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-19 16:23:37 +0200

    feat(fish): remove deprecated functions

--- config/fish/user-bindings.fish
+++ config/fish/user-bindings.fish
@@ -1,8 +1,5 @@
 bind \e\[106\;9u "_fzf_find $HOME"
 bind \e\[108\;9u "_fzf_find_search '$HOME/.local/mount/onedrive/notes/programming/'"
 bind \e\[106\;10u "_fzf_find --noignorefile"
-bind \e\[108\;10u "_fzf_find_search"
-# bind \e\[111\;9u "switch_tabs"
-# bind \e\[105\;9u "extract_links '$HOME/.local/notes/other/work-websites.md'"
-bind \e\[117\;9u "extract_links '$HOME/.local/notes/other/websites.md'"
-# bind \e\[112\;9u 'glab-repos'
+# bind \e\[108\;10u "_fzf_find_search"
+bind \e\[107\;9u "_fzf_find_search"
