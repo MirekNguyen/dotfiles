fe26ca68f826a5808e38bf958eb8e72fa4293420
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-26 18:27:54 +0200

    feat: update install script

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ git clone git@github.com:MirekNguyen/dotfiles.git "$HOME"/.config/dotfiles
 cd "$HOME"/.config/dotfiles || exit
 stow -D -t "$HOME"/.config config
 stow -t "$HOME"/.config config
+fisher update
