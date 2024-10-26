df61d9ed2c36ee99fc804b20eeb314d6366bb264
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-26 16:28:50 +0200

    feat(fish): use fish folder for symlink

---
+++ config/fish/conf.d/user-config.fish
@@ -0,0 +1,38 @@
+# Starship
+if type -q starship
+  starship init fish | source
+end
+
+# XDG
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker 
+export HOMEBREW_CASK_OPTS="--appdir=/Applications/_Casks"
+export LESSHISTFILE=-
+
+set fish_greeting
+set -gx EDITOR nvim 
+set -gx MANPAGER "sh -c 'col -bx | bat --theme=gruvbox-dark -l man -p'"
+set -gx TERM xterm-256color
+
+# XDG
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_STATE_HOME "$HOME/.local/log"
+
+# Configuration
+set -gx LS_COLORS "di=36"
+set -gx BAT_THEME "gruvbox-dark"
+
+# PATH
+set -gx GOPATH "$HOME/.local/scripts/go"
+set -gx CARGO_HOME "$HOME/.local/scripts/cargo/"
+set -gx LIMA_HOME "$HOME/.config/lima"
+set -gx RLWRAP_HOME "$XDG_DATA_HOME/translate-shell/"
+set -gx PM2_HOME "$XDG_DATA_HOME/pm2"
+
+set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
+set -gx OPENAI_MODEL "gpt-4o-2024-08-06"
+set -gx VAULT_ADDR 'http://127.0.0.1:8200'
+# set -gx DOCKER_HOST "unix://$HOME/.config/colima/docker.sock"
