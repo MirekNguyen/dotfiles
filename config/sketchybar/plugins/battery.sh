f906b5b2591521b378f9409c11aeff644bb51a5c
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-28 15:06:48 +0100

    feat(sketchybar): color to battery

--- config/sketchybar/plugins/battery.sh
+++ config/sketchybar/plugins/battery.sh
@@ -2,6 +2,7 @@
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
+ICON_COLOR="0xffffffff"
 
 if [ "$PERCENTAGE" = "" ]; then
   exit 0
@@ -14,9 +15,9 @@ case "${PERCENTAGE}" in
   ;;
   [3-5][0-9]) ICON="􀺶"
   ;;
-  [1-2][0-9]) ICON="􀛩"
+  [1-2][0-9]) ICON="􀛩" ICON_COLOR=0xffffcc00
   ;;
-  *) ICON="􀛪"
+  *) ICON="􀛪" ICON_COLOR=0xffff3b30
 esac
 
 if [[ "$CHARGING" != "" ]]; then
@@ -25,4 +26,4 @@ fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.color="$ICON_COLOR"
