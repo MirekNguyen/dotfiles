e85b523e81f78b11c36f4844c543ed8898ff814d
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-28 16:02:18 +0100

    feat(sketchybar): media plugin update drawing

--- config/sketchybar/items/media.sh
+++ config/sketchybar/items/media.sh
@@ -4,7 +4,5 @@ sketchybar --add item media e \
            --set media label.max_chars=20 \
                        icon.padding_left=0 \
                        scroll_texts=on \
-                       icon=􀑪             \
-                       background.drawing=off \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
