a1c8a069df124f863fea38a1d04db837337fa3d1
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-10-29 18:34:14 +0100

    feat: change frequency of wifi

--- config/sketchybar/items/wifi.sh
+++ config/sketchybar/items/wifi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sketchybar --add item wifi right \
-           --set wifi update_freq=10 script="$PLUGIN_DIR/wifi.sh"
+           --set wifi update_freq=5 script="$PLUGIN_DIR/wifi.sh"
