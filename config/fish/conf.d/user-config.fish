b41ec550a87567cdb63a8b178ea81c7fa0f3caec
Mirek Nguyen <mirek.nguyen@o2.cz>
2024-12-22 00:39:09 +0100

    feat: use fish shell

--- config/fish/conf.d/user-config.fish
+++ config/fish/conf.d/user-config.fish
@@ -1,4 +1,53 @@
-set -U fish_user_paths "$HOME"/.local/go/bin "$HOME"/.local/cargo/bin "$HOME"/.local/bin "$HOME"/.config/dotfiles/scripts /Users/mireknguyen/.nix-profile/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin /opt/local/bin /opt/homebrew/bin /opt/homebrew/opt/fzf/bin $fish_user_paths
+set fish_greeting
+set -U fish_user_paths "$HOME/.config/dotfiles/scripts/" /Users/mireknguyen/.dotnet/tools /Users/mireknguyen/.nix-profile/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin /opt/local/bin /opt/homebrew/bin /opt/homebrew/opt/fzf/bin $fish_user_paths
+
+# aliases
+alias v='nvim'
+alias fzf "fzf -e --height 30% --border rounded"
+alias docker-ps "docker ps -a --format 'table {{.ID}}|{{.Names}}|{{.Status}}|{{.Ports}}' | column -t -s '|'"
+alias c 'click.sh "$HOME"/.local/secrets/websites'
+alias k 'kubectl'
+alias kn 'kubens'
+alias kx 'kubectx'
+alias ls='eza -g --oneline'
+alias l='eza -g -la'
+alias db 'nvim -c "set filetype=sql" -c "DBUIToggle"'
+
+# ssh, mount
+alias sshlx 'ssh-servers.sh "$HOME"/.local/secrets/work-servers.json'
+alias sshs 'ssh-servers.sh "$HOME"/.local/secrets/home-servers.json'
+alias mnt 'smb-servers.sh "$HOME"/.local/secrets/smb-servers.json'
+alias nix-rebuild 'darwin-rebuild switch --flake ~/.config/dotfiles/mac#mira'
+
+# XDG
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_STATE_HOME "$HOME/.local/log"
+
+# Configuration
+set -gx EDITOR nvim 
+set -gx TERM xterm-256color
+set -gx LS_COLORS "di=36"
+set -gx GOPATH "$HOME/.local/scripts/go"
+set -gx CARGO_HOME "$HOME/.local/scripts/cargo/"
+set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
+
+set -gx LIMA_HOME "$HOME/.config/lima"
+set -gx DOCKER_HOST "unix://$HOME/.config/colima/docker.sock"
+set -gx DOCKER_CONFIG "$XDG_CONFIG_HOME"/docker
+set -gx KUBECONFIG "$XDG_CONFIG_HOME/kube" 
+set -gx KUBECACHEDIR "$XDG_CACHE_HOME/kube"
+
+bind \e\[106\;9u "_fzf_find $HOME"
+bind \e\[108\;9u "_fzf_find_search '$HOME/.local/mount/onedrive/notes/programming/'"
+bind \e\[106\;10u "_fzf_find --noignorefile"
+bind \e\[108\;10u "_fzf_find_search"
+
+# Starship
+if type -q starship
+  starship init fish | source
+end
 
 # Loading licence keys from .env
 function envsource
@@ -9,45 +58,4 @@ function envsource
     end
   end
 end
-envsource "$HOME/.config/dotfiles/config/fish/.env"
 envsource "$HOME/.local/secrets/environment"
-
-# Starship
-if type -q starship
-  starship init fish | source
-end
-
-
-# XDG
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications/_Casks"
-export LESSHISTFILE=-
-
-set fish_greeting
-set -gx EDITOR nvim 
-set -gx MANPAGER "sh -c 'col -bx | bat --theme=gruvbox-dark -l man -p'"
-set -gx TERM xterm-256color
-
-# XDG
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_STATE_HOME "$HOME/.local/log"
-
-# Configuration
-set -gx LS_COLORS "di=36"
-set -gx BAT_THEME "gruvbox-dark"
-
-# PATH
-set -gx GOPATH "$HOME/.local/scripts/go"
-set -gx CARGO_HOME "$HOME/.local/scripts/cargo/"
-set -gx LIMA_HOME "$HOME/.config/lima"
-set -gx RLWRAP_HOME "$XDG_DATA_HOME/translate-shell/"
-set -gx PM2_HOME "$XDG_DATA_HOME/pm2"
-
-set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
-set -gx OPENAI_MODEL "gpt-4o-2024-08-06"
-set -gx VAULT_ADDR 'http://127.0.0.1:8200'
-# set -gx DOCKER_HOST "unix://$HOME/.config/colima/docker.sock"
---
+++ config/fish/conf.d/user-work.fish
@@ -0,0 +1,33 @@
+#!/usr/bin/env fish
+
+function work-vpn-connect
+    echo $WORK_VPN_PASSWORD | sudo openconnect --passwd-on-stdin --background -c /Users/mireknguyen/.local/secrets/o2-cz.p12 -s 'vpn-slice 10.0.0.0/8 172.26.193.0/24' zamevpn.o2.cz --servercert pin-sha256:pUMlm+u8GjTUgL8GiyTG+odQaUMD9FGpQQNTki/pTgQ=
+end
+
+function work-vpn-disconnect
+    sudo pkill -9 openconnect
+
+    # Flush DNS
+    sudo dscacheutil -flushcache
+    sudo killall -HUP mDNSResponder
+    networksetup -setairportpower en0 off
+    networksetup -setairportpower en0 on
+
+    # Route flush
+    networksetup -setairportpower en0 off
+    sudo route flush
+    sudo ifconfig en1 down
+    sudo ifconfig en1 up
+    networksetup -setairportpower en0 on
+end
+
+function work-vpn
+    set option (gum choose "connect" "disconnect")
+    switch $option
+        case "connect"
+            work-vpn-connect
+        case "disconnect"
+            work-vpn-disconnect
+    end
+end
+
