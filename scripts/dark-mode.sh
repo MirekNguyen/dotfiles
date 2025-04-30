8980bc5628c73a6db18819aa7dbeee1a37092cdb
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2025-04-30 23:12:10 +0200

    fix: Update dark-mode script to handle input choice

--- scripts/dark-mode.sh
+++ scripts/dark-mode.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-choice="$(gum choose "dark" "light")"
-
-if [ "$choice" = "light" ]; then
-  kitty +kitten themes --reload-in=all 'Gruvbox Material Light Soft' &&
-  osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false'
+if [ -n "$1" ]; then
+  choice="$1"
 else
-  kitty +kitten themes --reload-in=all 'Gruvbox Material Dark Medium' &&
-  osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
+  choice="$(gum choose "dark" "light")"
 fi
 
+if [ "$choice" = "light" ]; then
+  kitty @ kitten themes --reload-in=all 'Gruvbox Material Light Soft';
+  osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false';
+elif [ "$choice" = "dark" ]; then
+  kitty @ kitten themes --reload-in=all 'Gruvbox Material Dark Medium';
+  osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true';
+fi
