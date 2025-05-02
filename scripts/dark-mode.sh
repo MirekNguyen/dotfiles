e4ce9d5f9cc58ca05a3a888a559a9600c60552ca
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2025-05-02 20:17:18 +0200

    style: Remove unnecessary kitty theme reload commands

--- scripts/dark-mode.sh
+++ scripts/dark-mode.sh
@@ -7,9 +7,7 @@ else
 fi
 
 if [ "$choice" = "light" ]; then
-  kitty @ kitten themes --reload-in=all 'Gruvbox Material Light Soft';
   osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false';
 elif [ "$choice" = "dark" ]; then
-  kitty @ kitten themes --reload-in=all 'Gruvbox Material Dark Medium';
   osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true';
 fi
