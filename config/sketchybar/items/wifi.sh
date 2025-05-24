d72359d9894ce00f451a6ae32ba36a3c19c5361a
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2025-05-24 06:19:31 +0200

    feat: Add new PHP 8.3 aliases and update WiFi sketchybar script

--- config/sketchybar/items/wifi.sh
+++ config/sketchybar/items/wifi.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 sketchybar --add item wifi right \
-           --set wifi update_freq=5 script="$PLUGIN_DIR/wifi.sh"
+           --set wifi script="$PLUGIN_DIR/wifi.sh" \
+           --subscribe wifi wifi_change
+           # --set wifi update_freq=5 script="$PLUGIN_DIR/wifi.sh"
